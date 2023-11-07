56da41dace272660a44db6ea3269f068494c0a2b
Dharmender <dharmenderarya477@gmail.com>
2023-11-07 23:52:12 +0530

    Big Basket Clone

---
+++ BigBasket/body/script.js
@@ -0,0 +1,41 @@
+// JS code for slider 
+const imgArr = [
+    "/BigBasket/bigBasketImg/Image File59.jpg" , 
+    "/BigBasket/bigBasketImg/Image File60.jpg" , 
+    "/BigBasket/bigBasketImg/Image File61.jpg" , 
+    "/BigBasket/bigBasketImg/Image File62.jpg" 
+]
+
+const slideImg = document.querySelector('#slideImg');
+let prev = document.querySelector('#prev');
+let next = document.querySelector('#next');
+
+slideImg.setAttribute('src'  , imgArr[1]);
+
+let currentImg = 0;
+prev.addEventListener('click' , () => {
+    currentImg--;
+    if (currentImg < 0) {
+        currentImg = imgArr.length-1;
+    }
+    slideImg.setAttribute('src' , imgArr[currentImg])
+})
+next.addEventListener('click' , () => { 
+    currentImg++;
+    if (currentImg >= imgArr.length) {
+        currentImg = 0;
+    }
+    slideImg.setAttribute('src' , imgArr[currentImg])
+})
+
+// Function to automate the image change
+function autoSlide() {
+    currentImg++;
+    if (currentImg >= imgArr.length) {
+        currentImg = 0;
+    }
+    slideImg.setAttribute('src', imgArr[currentImg]);
+}
+
+// Set interval to automatically change images every 3 seconds (adjust the interval time as needed)
+const interval = setInterval(autoSlide, 2000);
